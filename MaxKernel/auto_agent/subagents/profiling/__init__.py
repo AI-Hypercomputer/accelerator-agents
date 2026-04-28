d788edaeb136678d98bb1b6d2a1a936a687abbfd
shangkunwang <shangkunwang@google.com>
2026-04-28 21:15:38 +0000

    refactor: remove unused read_file_for_profiling_agent from imports and __all__ list

--- MaxKernel/auto_agent/subagents/profiling/__init__.py
+++ MaxKernel/auto_agent/subagents/profiling/__init__.py
@@ -4,13 +4,11 @@ from .agent import (
   eval_profile_agent,
   generate_profiling_script_agent,
   profile_agent,
-  read_file_for_profiling_agent,
   summarize_profile_agent,
 )
 
 __all__ = [
   "profile_agent",
-  "read_file_for_profiling_agent",
   "generate_profiling_script_agent",
   "eval_profile_agent",
   "summarize_profile_agent",

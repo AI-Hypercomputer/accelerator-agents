0de7cd0c9e19a04ca4d865585730812fd675c94e
stingram <steven.e.ingram@gmail.com>
2026-09-15 22:56:57 +0000

    bug fix

--- MaxKernel/auto_agent/server_utils/tpu_server.py
+++ MaxKernel/auto_agent/server_utils/tpu_server.py
@@ -517,7 +517,11 @@ def get_tpu_version() -> dict:
 if __name__ == "__main__":
   port_env = os.environ.get("PORT")
   if port_env:
-    tpu_port = int(port_env)
+    try:
+      tpu_port = int(port_env)
+    except ValueError:
+      logging.error(f"Invalid PORT environment variable: {port_env}. Must be an integer.")
+      sys.exit(1)
   else:
     tpu_port = get_local_tpu_port()
 
